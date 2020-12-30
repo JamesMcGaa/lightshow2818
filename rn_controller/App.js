1e60f804438356a35ef7ff7ae41c28b22dbde731
James McGaa <jemcgaa@mit.edu>
2020-12-29 23:01:24 -0500

    Cleaning up controller code

--- rn_controller/App.js
+++ rn_controller/App.js
@@ -1,55 +1,97 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import Slider from "@react-native-community/slider";
+import Slider from '@react-native-community/slider';
+import { Picker } from '@react-native-picker/picker';
+import React, { useState } from 'react';
 import {
   ImageBackground,
-  Switch,
-  Button,
-  StyleSheet,
+  StyleSheet, Switch,
   Text,
   View,
-} from "react-native";
-import { Picker } from "@react-native-picker/picker";
-import { ColorPicker, toHsv, fromHsv } from "react-native-color-picker";
-import axios from "axios";
-const cors = require('cors');
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+} from 'react-native';
+import { ColorPicker, fromHsv } from 'react-native-color-picker';
 import { throttle } from 'throttle-debounce';
 
+const FlaskPrefix = '10.0.0.8:5000';
+const MinBrightness = 3;
+const MaxBrightness = 50;
+const ModesEnum = Object.freeze({ STATIC: 'STATIC', RAINBOW: 'RAINBOW' });
+const RedisKeysEnum = Object.freeze({
+  BRIGHTNESS: 'BRIGHTNESS', POWER: 'POWER', MODE: 'MODE', HEX: 'HEX',
+});
+const PowerOn = 'TRUE';
+const PowerOff = 'FALSE';
+const GREY = '#767577';
+const LIGHT_BLUE = '#81b0ff';
+const SliderExponentiation = 10;
+const ThrottleTimeoutms = 100;
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    fontSize: 20,
+    margin: 30,
+    marginTop: 80,
+  },
+  label: {
+    fontSize: 20,
+    marginRight: 20,
+    textAlignVertical: 'center',
+  },
+  row: {
+    marginBottom: 30,
+    flexDirection: 'row',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  modePicker: {
+    width: 200,
+    height: 50,
+  },
+  colorPicker: {
+    width: '100%',
+    height: 300,
+  },
+  brightnessSlider: {
+    transform: [{ scaleY: 2 }],
+  },
+  background: {
+    opacity: 0.3,
+  },
+});
 
 export default function App() {
-  const [isEnabled, setIsEnabled] = useState(true);
-  const [brightness, setBrightness] = useState(3);
-  const [mode, setMode] = useState("RAINBOW");
-
-  function sliderHandler(event) {
-    let scaled_brightness = Math.round(Math.pow(event, 10));
-    setBrightness(scaled_brightness);
-    set("BRIGHTNESS", scaled_brightness.toString());
-  }
-
   async function set(param, value) {
-    console.log(`http://10.0.0.8:5000/set/${param}/${value}`);
-    await fetch(`http://10.0.0.8:5000/set/${param}/${value}`, { mode: 'no-cors' })
+    await fetch(`http://${FlaskPrefix}/set/${param}/${value}`, { mode: 'no-cors' })
       .then((response) => console.log('successful fetchToken response: ', response))
       .catch((error) => console.log('fetchToken error: ', error));
   }
 
-  const throttled_set = throttle(100, true, set);
+  async function sliderHandler(event) {
+    const scaledBrightness = Math.round(event ** SliderExponentiation);
+    await set(RedisKeysEnum.BRIGHTNESS, scaledBrightness.toString());
+  }
+
+  const [isEnabled, setIsEnabled] = useState(true);
+  const [mode, setMode] = useState(ModesEnum.RAINBOW);
+  const throttedSet = throttle(ThrottleTimeoutms, true, set);
 
   return (
     <View style={{ flex: 1 }}>
-      <ImageBackground source={require("./assets/winter.jpg")} imageStyle=
-        {{ opacity: 0.3 }} style={styles.image}>
+      <ImageBackground
+        source={require('./assets/winter.jpg')}
+        imageStyle={styles.background}
+        style={styles.image}
+      >
         <View style={styles.container}>
           <View style={styles.row}>
             <Text style={styles.label}>Power</Text>
             <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              trackColor={{ false: GREY, true: LIGHT_BLUE }}
               onValueChange={async () => {
-                await set("POWER", isEnabled ? "FALSE" : "TRUE");
+                await set(RedisKeysEnum.POWER, isEnabled ? PowerOff : PowerOn);
                 setIsEnabled(!isEnabled);
               }}
               value={isEnabled}
@@ -59,13 +101,13 @@ export default function App() {
           <View style={styles.row}>
             <Text style={styles.label}>Brightness</Text>
             <Slider
-              style={{ transform: [{ scaleY: 2 }] }}
+              style={styles.brightnessSlider}
               flex={1}
-              minimumValue={Math.pow(3, 0.1)}
-              maximumValue={Math.pow(255, 0.1)}
-              minimumTrackTintColor="blue"
+              minimumValue={MinBrightness ** (1.0 / SliderExponentiation)}
+              maximumValue={MaxBrightness ** (1.0 / SliderExponentiation)}
+              minimumTrackTintColor={GREY}
               thumbTintColor="blue"
-              maximumTrackTintColor="#CCCCCC"
+              maximumTrackTintColor={LIGHT_BLUE}
               onSlidingComplete={sliderHandler}
             />
           </View>
@@ -74,55 +116,27 @@ export default function App() {
             <Text style={styles.label}>Mode</Text>
             <Picker
               selectedValue={mode}
-              style={{ width: 200, height: 50 }}
+              style={styles.modePicker}
               onValueChange={async (modeValue, ModeIndex) => {
-                await set("MODE", modeValue);
+                await set(RedisKeysEnum.MODE, modeValue);
                 setMode(modeValue);
               }}
             >
-              <Picker.Item label="STATIC" value="STATIC" />
-              <Picker.Item label="RAINBOW" value="RAINBOW" />
+              <Picker.Item label={ModesEnum.STATIC} value={ModesEnum.STATIC} />
+              <Picker.Item label={ModesEnum.RAINBOW} value={ModesEnum.RAINBOW} />
             </Picker>
           </View>
 
-          {mode == "STATIC" ? <ColorPicker
-            onColorChange={async (color) => {
-              console.log(fromHsv(color));
-              await throttled_set("HEX", fromHsv(color).substring(1));
-            }}
-            style={{ width: "100%", height: 300 }}
-          /> : null}
-
-          <StatusBar style="auto" />
+          {mode === ModesEnum.STATIC ? (
+            <ColorPicker
+              onColorChange={async (color) => {
+                await throttedSet(RedisKeysEnum.HEX, fromHsv(color).substring(1));
+              }}
+              style={styles.colorPicker}
+            />
+          ) : null}
         </View>
       </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-
-    alignItems: "flex-start",
-    fontSize: 20,
-    margin: 30,
-    marginTop: 80,
-  },
-
-  label: {
-    fontSize: 20,
-    marginRight: 20,
-    textAlignVertical: "center",
-  },
-
-  row: {
-    marginBottom: 30,
-    flexDirection: "row",
-  },
-  image: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-  },
-});
